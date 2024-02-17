7b6485ee89ea69e95258d681301f20190fddb712
Katrina Pingryn <katep@indeed.com>
2024-02-17 13:40:00 -0600

    cors back end

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 const corsEnabledRouter = require("./cors-enabled/cors-enabled.router");
@@ -7,6 +8,7 @@ const corsNotEnabledRouter = require("./cors-not-enabled/cors-not-enabled.router
 const notFound = require("./errors/notFound");
 const errorHandler = require("./errors/errorHandler");
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/cors-enabled", corsEnabledRouter);
